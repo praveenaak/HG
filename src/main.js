078b054b3af7b7d5d517cdf8794c3138c712d438
praveenaak <praveenaa2408@gmail.com>
2023-10-15 22:53:05 -0700

    added styling changes

--- src/main.js
+++ src/main.js
@@ -3,17 +3,55 @@ var hexLayerGroup, choroplethLayerGroup;
 var selectedHexLayer;
 var miniMap; 
 var neighborsMap;
+var jsonData; 
+var hexMap; 
+var choroplethMap;
+
+// Function to determine the color for a feature based on the choropleth scale
+function getColorForFeature(feature) {
+    var pollutantSelector = document.getElementById('pollutant-selector');
+    var pollutantType = pollutantSelector.value; 
+    var value = feature.properties[pollutantType.toUpperCase()]; // The actual value for this feature
+
+    // Assume you have a global or otherwise accessible set of your min and max values for the pollutant
+    var minPollutantValue = Number.POSITIVE_INFINITY;
+    var maxPollutantValue = Number.NEGATIVE_INFINITY;
+
+    return getColor(value, minPollutantValue, maxPollutantValue); // This function should already be defined in your code
+}
+
+// Function to normalize data values into a 0-1 range
+function normalize(value, min, max) {
+    return (value - min) / (max - min);
+}
+
+// Function to create a d3 color scale similar to 'viridis'
+function createColorScale() {
+    var scale = d3.scaleSequential()
+        .domain([0, 1]) // Domain is normalized to 0-1 range
+        .interpolator(d3.interpolateViridis); // Using Viridis color scheme
+    return scale;
+}
+
+// Function to get a color based on a value, using the scale
+function getColor(value, min, max) {
+    var colorScale = createColorScale();
+    var normalizedValue = normalize(value, min, max);
+    return colorScale(normalizedValue);
+}
+
 
 // Function to initialize the hexagon map
 function drawHexMap() {
-    // Initialize the map
-    var map = L.map('hex-map', {
+    hexMap = L.map('hex-map', { // make sure 'hex-map' is the correct container ID
         center: [41.6032, -73.0877], // Coordinates for Connecticut
         zoom: 10,
         layers: [],
         zoomControl: false,
         attributionControl: false,
     });
+    document.getElementById('hex-map').style.backgroundColor = 'transparent';
+
 
     document.getElementById('hex-map').style.backgroundColor = 'transparent';
 
@@ -61,62 +99,89 @@ function drawHexMap() {
                         displaySelectedInThirdBox(geoid);
                     });
                 }
-            }).addTo(map);
+            }).addTo(hexMap);
 
-            map.fitBounds(hexLayerGroup.getBounds());
+            hexMap.fitBounds(hexLayerGroup.getBounds());
         })
         .catch(error => console.error('Error loading hexagon GeoJSON data:', error));
 }
 
 function drawChoroplethMap() {
-    // Initialize the map without a tile layer and with a transparent background
-    var map = L.map('map', {
+    if (choroplethMap) {
+        choroplethMap.remove(); // This will clear the existing map
+    }
+
+    // Now, we recreate the choroplethMap
+    choroplethMap = L.map('map', { 
         center: [33.44, -112.07], // Coordinates for Phoenix
-        zoom: 5 , // This zoom level can be adjusted
-        zoomControl: true, // If you want, you can set this to false to hide zoom controls
-        attributionControl: false, // Hide default Leaflet attribution
+        zoom: 5,
+        zoomControl: true,
+        attributionControl: false,
     });
 
-
-    // Fetch the choropleth GeoJSON data
+    // Here, we fetch the data again (or access the new pollutant data if already fetched)
     fetch('../data/AQI_CT_imputed.geojson')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Use 'data' directly. Remove references to 'jsonData'.
+        if (!choroplethMap) { // Just check if 'map' is initialized, as 'data' is now in scope
+            console.error('Map has not been initialized.');
+            return;
+        }
+    
+        // Clear any existing layers
+        choroplethMap.eachLayer(layer => {
+            if (layer instanceof L.GeoJSON) { // Check if layer is a GeoJSON layer, to avoid removing tile layers
+                choroplethMap.removeLayer(layer);
             }
-            return response.json();
-        })
-        .then(data => {
-            function getColor(ozValue) {
-                return ozValue > 0.05 ? '#800026' :
-                       ozValue > 0.04 ? '#BD0026' :
-                       ozValue > 0.03 ? '#E31A1C' :
-                       ozValue > 0.02 ? '#FC4E2A' :
-                       ozValue > 0.01 ? '#FD8D3C' :
-                       ozValue > 0    ? '#FEB24C' :
-                                        '#FFEDA0';
-            }
-
-            function style(feature) {
-                return {
-                    color: 'white', // Border color for the choropleth shapes
-                    fillColor: getColor(feature.properties.OZ), // Fill based on the OZ value
-                    fillOpacity: 0.5,
-                    weight: 2 // Border thickness
-                };
-            }
-
-            // Create a GeoJSON layer and add it to the map
-            choroplethLayerGroup = L.geoJson(data, {
-                style: style,
-                onEachFeature: function(feature, layer) {
-                    layer.bindPopup(feature.properties.NAMELSAD + '<br>OZ: ' + feature.properties.OZ);
-                }
-            }).addTo(map);
-        })
-        .catch(error => {
-            console.error('Fetch error:', error);
         });
+    
+        // Get the selected pollutant from the dropdown
+        var pollutantSelector = document.getElementById('pollutant-selector');
+        var pollutantType = pollutantSelector.value; 
+    
+        let minPollutantValue = Number.POSITIVE_INFINITY;
+        let maxPollutantValue = Number.NEGATIVE_INFINITY;
+
+        // Loop through all features to find the actual minimum and maximum values
+        data.features.forEach((feature) => {
+            let val = feature.properties[pollutantType.toUpperCase()];
+            if (val < minPollutantValue) minPollutantValue = val;
+            if (val > maxPollutantValue) maxPollutantValue = val;
+        });
+
+        // 2. Apply the color based on each feature's value
+        function style(feature) {
+            var value = feature.properties[pollutantType.toUpperCase()]; // The actual value for this feature
+
+            // Get color based on the value, using the scale
+            var color = getColor(value, minPollutantValue, maxPollutantValue);
+
+            return {
+                color: 'white', // Border color for the choropleth shapes
+                fillColor: color, // Fill color based on the feature's specific value
+                fillOpacity: 0.7,
+                weight: 2 // Border thickness
+            };
+        }
+
+        // Create a GeoJSON layer and add it to the map
+        choroplethLayerGroup = L.geoJson(data, {
+            style: style,
+            onEachFeature: function(feature, layer) {
+                // Customize the popup content
+                layer.bindPopup(feature.properties.NAMELSAD + '<br>' + pollutantType + ': ' + feature.properties[pollutantType.toUpperCase()]);
+            }
+        }).addTo(choroplethMap);
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
 }
 
 function highlightCorrespondingChoropleth(geoid) {
@@ -149,6 +214,7 @@ function highlightCorrespondingChoropleth(geoid) {
     if (!found) {
         console.log("No matching layer found for GEOID:", geoid);
     }
+
 }
 
 function displaySelectedInThirdBox(geoid) {
@@ -190,13 +256,16 @@ function displaySelectedInThirdBox(geoid) {
             miniMap.removeLayer(selectedHexLayer);
         }
 
+        // Get the color for the selected feature based on its value from the choropleth map
+        var featureColor = getColorForFeature(correspondingFeature);
+
         selectedHexLayer = L.geoJson(correspondingFeature, {
             style: function() {
                 return {
-                    color: 'yellow',
+                    color: featureColor,  // Use the same color as on the choropleth map
                     weight: 2,
                     opacity: 1,
-                    fillColor: 'yellow',
+                    fillColor: featureColor,  // Use the same color as on the choropleth map
                     fillOpacity: 0.7
                 };
             }
@@ -211,6 +280,7 @@ function displaySelectedInThirdBox(geoid) {
         console.error('No matching feature found for GEOID:', geoid);
     }
 }
+
 
 
 // New function: Builds a table and populates it with the feature's properties
@@ -281,13 +351,16 @@ function displaySelectedWithNeighbors(geoid) {
             neighborsMap.removeLayer(layer);
         });
 
+        // Determine the color for the selected feature based on its value from the choropleth map
+        var selectedFeatureColor = getColorForFeature(selectedFeature);
+
         // Highlight the selected feature and add it to the layers array
         var selectedLayer = L.geoJSON(selectedFeature, {
             style: {
-                color: 'blue', // or whatever highlight color you prefer
+                color: selectedFeatureColor, // Use the color based on the choropleth scale
                 weight: 2,
                 opacity: 1,
-                fillColor: 'blue',
+                fillColor: selectedFeatureColor, // Use the color based on the choropleth scale
                 fillOpacity: 0.7
             }
         }).addTo(neighborsMap);
@@ -295,12 +368,14 @@ function displaySelectedWithNeighbors(geoid) {
 
         // Display the neighbors and add them to the layers array
         neighbors.forEach(function(neighborFeature) {
+            var neighborColor = getColorForFeature(neighborFeature); // Get color for this neighbor
+
             var neighborLayer = L.geoJSON(neighborFeature, {
                 style: {
-                    color: 'green', // or a different color to differentiate from the selected feature
+                    color: neighborColor, // Color based on the choropleth scale
                     weight: 1,
                     opacity: 0.7,
-                    fillColor: 'green',
+                    fillColor: neighborColor, // Color based on the choropleth scale
                     fillOpacity: 0.5
                 }
             }).addTo(neighborsMap);
@@ -315,8 +390,14 @@ function displaySelectedWithNeighbors(geoid) {
     }
 }
 
-// Execute when the DOM is fully loaded
+
+// Now, update your event listener for the dropdown change
 document.addEventListener('DOMContentLoaded', function() {
-    drawChoroplethMap();
+    drawChoroplethMap(); // Initial draw
     drawHexMap();
+
+    var pollutantSelector = document.getElementById('pollutant-selector');
+    pollutantSelector.addEventListener('change', function() {
+        drawChoroplethMap(); // This should now fully refresh the map with new data
+    });
 });
